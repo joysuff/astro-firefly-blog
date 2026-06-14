c31ae36f73e4c3d9b0eede460f57cef0fb1257ba
joysuff <3384214519@qq.com>
2026-06-14 20:21:47 +0800

    修改站点域名，配置umami

--- src/config/analyticsConfig.ts
+++ src/config/analyticsConfig.ts
@@ -8,11 +8,11 @@ export const analyticsConfig: AnalyticsConfig = {
 	// Umami 统计配置
 	umamiAnalytics: {
 		// Umami Website ID
-		websiteId: "",
+		websiteId: "e181f1bb-c9cc-4c4d-8332-c1d29df36b2d",
 		// Umami JS地址，支持使用自建
-		scriptUrl: "https://cloud.umami.is/script.js",
+		scriptUrl: "https://umami.joysuff.cc.cd/script.js",
 		// Umami 会话回放脚本地址，支持使用自建
-		replaysScriptUrl: "https://cloud.umami.is/recorder.js",
+		replaysScriptUrl: "https://umami.joysuff.cc.cd/recorder.js",
 		// 是否追踪出站链接
 		trackOutboundLinks: true,
 		// 是否收集浏览器性能指标
--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -12,7 +12,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "终不似，少年游",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.joysuff.cc.cd",
 
 	// 站点描述
 	description:
